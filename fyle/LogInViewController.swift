bd2a1bb6f30e7d816b361d4ab46684d1d1e24e22
User@77 <user77@Admin77s-MacBook-Pro.local>
2024-11-05 11:05:30 +0530

    Initial Commit: uploading initial folders. Contains Splash Screen, Log In and Sign Up Screens

---
+++ fyle/LogInViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LogInViewController.swift
+//  fyle
+//
+//  Created by User@77 on 29/10/24.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fyleTests/fyleTests.swift
@@ -0,0 +1,17 @@
+//
+//  fyleTests.swift
+//  fyleTests
+//
+//  Created by User@77 on 28/10/24.
+//
+
+import Testing
+@testable import fyle
+
+struct fyleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
